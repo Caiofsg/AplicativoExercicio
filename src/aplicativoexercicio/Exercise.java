471546f473444fee51afaa759cf160b6de4cb287
Caio Fernando <66481755+Caiofsg@users.noreply.github.com>
2020-08-20 11:56:01 -0300

    Create Exercise.java

---
+++ src/aplicativoexercicio/Exercise.java
@@ -0,0 +1,58 @@
+package aplicativoexercicio;
+
+public class Exercise {
+    //Attributes
+    private String type, description, name;
+    private int countdown, repetiton;
+    
+    //Constructor
+    public Exercise(String n, String t, String d, int c){
+        this.setName(n);
+        this.setType(t);
+        this.setDescription(d);
+        this.setCountdown(c);
+    }
+    
+    //Getters e Setters
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getCountdown() {
+        return countdown;
+    }
+
+    public void setCountdown(int countdown) {
+        this.countdown = countdown;
+    }
+
+    public int getRepetiton() {
+        return repetiton;
+    }
+
+    public void setRepetiton(int repetiton) {
+        this.repetiton = repetiton;
+    }
+    
+}
