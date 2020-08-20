99499f4ebadbaee0e2fe1d6735c79d4c740c8a6b
Caio Fernando <66481755+Caiofsg@users.noreply.github.com>
2020-08-20 11:55:55 -0300

    Create Routine.java

---
+++ src/aplicativoexercicio/Routine.java
@@ -0,0 +1,15 @@
+package aplicativoexercicio;
+
+public class Routine {
+    //Attributes
+    private int week, time;
+    private float initial, meta;
+    private User person;
+    private Exercise v[];
+    private String objective;
+    
+    //Constructor
+    public Routine(User p, String ob){
+        
+    }
+}
