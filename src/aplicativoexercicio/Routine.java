b5c62e43f81ee91ff5b7348149ddf77a4dfad72c
Caio Fernando <66481755+Caiofsg@users.noreply.github.com>
2020-08-25 11:28:48 -0300

    Update Routine.java
    Português:
    Foi a criada a classe "Routine" que servirá como uma rotina de exercícios oferecida para o usuário após o cadastro do mesmo. A Routine e os exercícios contidos na mesma irão variar de acordo com o objetivo do usuário em questão. Na próxima atualiza ção será criada uma variável global que irá conter os exercícios a serem inseridos na Routine, futuramente dita variável será substituída por um banco de dados .
    
    English:
    Created the routine class wich will bserve as the set of exercises offered  by the app to the user. The routine will vary with the objective of the user in question. The next step will have the global variable that will be used to create the vectors containing the exercises that will be inserted in the routine, in the near future said variable will be replaced with a database.

--- src/aplicativoexercicio/Routine.java
+++ src/aplicativoexercicio/Routine.java
@@ -5,11 +5,81 @@ public class Routine {
     private int week, time;
     private float initial, meta;
     private User person;
-    private Exercise v[];
+    private Exercise v[], em[];
     private String objective;
     
     //Constructor
     public Routine(User p, String ob){
+        this.setPerson(p);
+        this.setObjective(ob);
+    }
+    
+    //Getters e Setters
+    public int getWeek() {
+        return week;
+    }
+
+    public void setWeek(int week) {
+        this.week = week;
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+    public void setTime(int time) {
+        this.time = time;
+    }
+
+    public float getInitial() {
+        return initial;
+    }
+
+    public void setInitial(float initial) {
+        this.initial = initial;
+    }
+
+    public float getMeta() {
+        return meta;
+    }
+
+    public void setMeta(float meta) {
+        this.meta = meta;
+    }
+
+    public User getPerson() {
+        return person;
+    }
+
+    public void setPerson(User person) {
+        this.person = person;
+    }
+
+    public Exercise[] getV() {
+        return v;
+    }
+
+    public void setV(Exercise[] v) {
+        this.v = v;
+    }
+
+    public String getObjective() {
+        return objective;
+    }
+
+    public void setObjective(String objective) {
+        this.objective = objective;
+    }
+    
+    //Métodos Manipuladores
+    
+    public void montarRoutine(){
+        this.getObjective();
+        if(this.objective == "Emagrecer"){
         
+        }
+        if(this.objective == "Ganhar Músculos"){
+        
+        }
     }
 }
